72a9365c3ee1cd17b54f4cced883b12a1f668ede
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-27 05:57:45 +0000

    Complete Javascript exercise: Exercise 06 - repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringValue, number) {
+    if (number < 0) return "ERROR";
+    let finalRes = "";
+    for (let i = 0; i < number; i++) {
+        finalRes += stringValue;
+    }
+    return finalRes
 };
 
 // Do not edit below this line
