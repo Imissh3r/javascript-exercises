8d9b23e54ca735f0c1209f42080d5250885ae029
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-28 12:34:15 +0000

    Complete Javascript exercise: Exercise 09 - sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (...arr) {
+    if ((typeof arr[0] === 'number' && typeof arr[1] === 'number') && (Number.isInteger(arr[0]) && Number.isInteger(arr[1])) && (arr[0] >= 0 && arr[1] >= 0)) {
+        const min = Math.min(...arr);
+        const max = Math.max(...arr);
+        return (max * (max + 1) / 2) - ((min - 1) * min / 2);
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
