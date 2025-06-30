bf707139931aaae1ad3f45d3d086a8f26a301b51
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-30 20:01:30 +0000

    Complete Javascript exercise: Exercise 14 - Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    let n = Number.parseInt(val);
+    return (n >= 0)? Math.round((Math.pow(1 + Math.sqrt(5), n) - Math.pow(1 - Math.sqrt(5), n)) / (Math.pow(2, n) * Math.sqrt(5))) : "OOPS";
 };
 
 // Do not edit below this line
