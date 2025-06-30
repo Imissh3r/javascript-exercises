30d4af5679296fac1324650397e95a7211e306ca
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-30 20:34:53 +0000

    Complete Javascript exercise: Exercise 16 - Find the Oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const deathYear = person.yearOfDeath || currentYear;
+        const age = deathYear - person.yearOfBirth;
+        return (age > oldest.age) ? {name: person.name, age: age} : oldest;
+    }, {name: "", age: 0});
 };
 
 // Do not edit below this line
