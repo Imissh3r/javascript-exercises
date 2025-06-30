50fbbf55cb7a6587468572999a27056d139af2dd
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-30 19:47:46 +0000

    Complete Javascript exercise: Exercise 13 - Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    return cleanStr === cleanStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
