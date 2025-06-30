13c0ae837d2263a7c9b22a7bdaae69e3d1b7a676
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-06-30 20:12:36 +0000

    Complete Javascript exercise: Exercise 15 - Get the Titles!

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((arr, value) => {
+        arr.push(value.title);
+        return arr;
+    }, []);
 };
 
 // Do not edit below this line
